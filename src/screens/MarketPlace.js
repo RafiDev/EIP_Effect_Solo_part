5fe87de757a0b268dc56a04e487c36f2efa99a04
RafiDev <yohann.desravines@epitech.eu>
2021-09-09 15:20:42 +0200

    Start add effect to playlist from marketplace

--- src/screens/MarketPlace.js
+++ src/screens/MarketPlace.js
@@ -1,5 +1,110 @@
 import * as React from 'react';
-import { SectionList, StyleSheet, Text, View } from 'react-native';
+import { Alert, Modal, Pressable, SectionList, StyleSheet, Text, View } from 'react-native';
+
+const DATA = [
+  {
+    title: 'EFFETS DE SATURATIONS',
+    data: [
+      {id: '1', value: 'Overdrive'},
+      {id: '2', value: 'Distorsion'},
+      {id: '3', value: 'Fuzz'}
+    ],
+  },
+  {
+    title: 'EFFETS DE FILTRES',
+    data: [
+      {id: '4', value: 'équaliseur'},
+      {id: '5', value: 'wah-wah'},
+      {id: '6', value: 'auto-wah'}
+    ],
+  },
+  {
+    title: 'EFFETS TEMPORELS',
+    data: [
+      {id: '7', value: 'Delay'},
+      {id: '8', value: 'Reverb'}
+    ]
+  },
+  {
+    title: 'EFFETS DE MODULATIONS',
+    data: [
+      {id: '9', value: 'Chorus'},
+      {id: '10', value: 'Flanger'},
+      {id: '11', value: 'Harmoniser'},
+      {id: '12', value: 'Tremolo'},
+      {id: '13', value: 'Whammy'}
+    ]
+  },
+  {
+    title: 'EFFETS DYNAMIQUES',
+    data: [
+      {id: '14', value: 'Compression'},
+      {id: '15', value: 'Noise Gate'},
+      {id: '16', value: 'Limiteur'},
+      {id: '17', value: 'Enhancer'}
+    ]
+  },
+];
+
+class MarketPlace extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+    };
+  };
+
+  displayModal(show) {
+    this.setState({modalVisible: show})
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({item}) => (
+              <>
+                <Modal
+                  presentationStyle="pageSheet"
+                  animationType="slide"
+                  visible={this.state.modalVisible}
+                  onRequestClose={() => {
+                    Alert.alert("Modal has now been closed.");
+                  }}
+                >
+                  <View style={styles.centeredView}>
+                    <Text style={styles.text}>Test</Text>
+                    <Pressable
+                      style={[styles.button, styles.buttonClose]}
+                      onPress={() => {this.displayModal(!this.state.modalVisible)}}
+                    >
+                      <Text style={styles.textStyle}>Fermer</Text>
+                    </Pressable>
+                  </View>
+                </Modal>
+                <Pressable
+                  onPress={() => this.displayModal(true)}
+                >
+                  <Text
+                    style={styles.item}
+                  >
+                    {item.value}
+                  </Text>
+                </Pressable>
+              </>
+            )}
+            renderSectionHeader={({section}) => (
+              <Text style={styles.sectionHeader}>
+                {section.title}
+              </Text>
+            )}
+          />
+        </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -20,67 +125,53 @@ const styles = StyleSheet.create({
       fontSize: 18,
       height: 44,
     },
+    text:{
+      padding: 10,
+  },
+  input:{
+      padding: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#2196F3",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
   })
 
-export default MarketPlace = () => {
-    let SATURATIONS = [
-        {id: '1', value: 'Overdrive'},
-        {id: '2', value: 'Distorsion'},
-        {id: '3', value: 'Fuzz'},
-    ];
-
-    let FILTRES = [
-        {id: '4', value: 'équaliseur'},
-        {id: '5', value: 'wah-wah'},
-        {id: '6', value: 'auto-wah'},
-    ];
-
-    let TEMPORELS = [
-        {id: '7', value: 'Delay'},
-        {id: '8', value: 'Reverb'},
-    ];
-
-    let MODULATION = [
-        {id: '9', value: 'Chorus'},
-        {id: '10', value: 'Flanger'},
-        {id: '11', value: 'Harmoniser'},
-        {id: '12', value: 'Tremolo'},
-        {id: '13', value: 'Whammy'},
-    ];
-
-    let DYNAMIQUE = [
-        {id: '14', value: 'Compression'},
-        {id: '15', value: 'Noise Gate'},
-        {id: '16', value: 'Limiteur'},
-        {id: '17', value: 'Enhancer'},
-    ];
-
-    return (
-      <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'EFFETS DE SATURATIONS', data: SATURATIONS},
-            {title: 'EFFETS DE FILTRES', data: FILTRES},
-            {title: 'EFFETS TEMPORELS', data: TEMPORELS},
-            {title: 'EFFETS DE MODULATION', data: MODULATION},
-            {title: 'EFFETS DYNAMIQUE', data: DYNAMIQUE},
-          ]}
-          renderSectionHeader={({section}) => (
-            <Text style={styles.sectionHeader}>
-              {section.title}
-            </Text>
-          )}
-          renderItem={({item}) => (
-            // Item for the FlatListItems
-            <Text
-              style={styles.item}
-              //Item Separator View
-              onPress={() => alert(JSON.stringify(item))}>
-              {item.value}
-            </Text>
-          )}
-          keyExtractor={(item, index) => index}
-        />
-      </View>
-    );
-}
+export default MarketPlace
